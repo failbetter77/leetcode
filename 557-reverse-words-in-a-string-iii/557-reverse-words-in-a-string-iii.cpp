535cf30a911a973c76bf77ae43dd0a0d8e4aa1eb
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-02-25 23:06:07 +0900

    Time: 19 ms (55.01%), Space: 9.8 MB (44.12%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l=0;
+        int r=0;
+        std::cout << s.length() << endl;
+        //auto p=s.begin();
+        while(r<=s.length()){
+            
+            //if((int(s[r])<=122 && int(s[r])>=97) || (int(s[r])>=65 && int(s[r])<=90)){
+            if(s[r]==' ' || r==s.length()){                
+                //reverse(s.begin()+l,s.begin()+r-1);
+                reverse(&s[l], &s[r]);
+                r++;
+                l=r;
+            }else{
+                r++;
+            }
+            
+        }
+     return s;   
+    }
+};
