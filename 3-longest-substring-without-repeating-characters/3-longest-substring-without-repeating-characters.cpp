8c491bb5b445c769c14cdde72c6985c78eeafdf1
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-02-28 14:32:37 +0900

    Time: 115 ms (12.65%), Space: 8.2 MB (72.13%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,10 +4,15 @@ public:
         vector<int> dict(256, -1);
         int maxLen = 0, start = -1;
         for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
+            if (dict[s[i]] > start){    // dict[s[i]] > start 를 체크하는 이유는,
+                                        // 이미 한번 봤던 숫자이므로, 시작점을 갱신하기 위해서다. 
+                cout << i << " : " <<start << " : " << dict[s[i]] << endl;
                 start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+                //cout << i << ": "<< s[i] << " : " <<start << endl;
+                
+            }
+            dict[s[i]] = i;     // dict[s[i]]에는 s내 알파벳의 위치가 들어간다. 
+            maxLen = max(maxLen, i - start);    // 길이를 세기위해, i에서 start를 뺀다. 
         }
         return maxLen;
     }
