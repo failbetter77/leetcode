51c7740a275be0bf371275936431123843ec839e
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-02-28 17:44:27 +0900

    Time: 65 ms (17.96%), Space: 8.4 MB (57.33%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // we will store a senitel value of -1 to simulate 'null'/'None' in C++
-        vector<int> chars(128, -1);
-
-        int left = 0;
-        int right = 0;
-
-        int res = 0;
-        while (right < s.length()) {
-            char r = s[right];
-
-            int index = chars[r];
-            if (index != -1 and index >= left and index < right) {
-                left = index + 1;
+        
+        int r=0;
+        int l=0;
+        int max=0;
+        vector<int> dict(256, -1);          // acsii코드에 대한 문자열 저장소를 만듬 index는 결국 문자이며,
+                                            // 해당값은 숫자값을 가진다. 
+        for(int i=0; i<s.size(); i++){            
+            if(dict[s[i]]<l){              // 처음 보는 알파벳일 경우
+                dict[s[i]]=i;               // 해당 알파벳의 index를 저장한다. 
+                cout << s[i] << endl;
             }
-            res = max(res, right - left + 1);
-
-            chars[r] = right;
-            right++;
+            else{
+                l=dict[s[i]]+1;
+                dict[s[i]]=i;
+            }                       
+            //cout << i << ":" << l << ":" << i-l+1 << endl;
+            max = max< i-l+1 ? i-l+1 : max;
+            
         }
-        return res;
+        return max;
     }
 };
