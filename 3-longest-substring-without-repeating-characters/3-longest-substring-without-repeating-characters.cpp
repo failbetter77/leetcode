50c2f696aab40385197318a33dd0755d1440de36
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-02-28 15:45:35 +0900

    Time: 46 ms (26.06%), Space: 10.7 MB (28.65%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start){    // dict[s[i]] > start 를 체크하는 이유는,
-                                        // 이미 한번 봤던 숫자이므로, 시작점을 갱신하기 위해서다. 
-                //cout << i << " : " <<start << " : " << dict[s[i]] << endl;
-                start = dict[s[i]];
-                //cout << i << ": "<< s[i] << " : " <<start << endl;
-                
-            }
-            dict[s[i]] = i;     // dict[s[i]]에는 s내 알파벳의 위치가 들어간다. 
-            maxLen = max(maxLen, i - start);    // 길이를 세기위해, i에서 start를 뺀다. 
-        }
-        return maxLen;
-    }
+	int lengthOfLongestSubstring(string s) 
+	{
+        
+        //version 2 
+		unordered_set<char> set;
+    
+		int i = 0, j = 0, n = s.size(), ans = 0;
+    
+		while( i<n && j<n)
+		{
+			if(set.find(s[j]) == set.end()) //If the character does not in the set
+			{
+				set.insert(s[j++]); //Insert the character in set and update the j counter
+				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
+			}
+			else
+			{
+				set.erase(s[i++]); 
+				/*If character does exist in the set, ie. it is a repeated character, 
+				we update the left side counter i, and continue with the checking for substring. */
+			}
+		}
+    
+		return ans;
+	}
 };
