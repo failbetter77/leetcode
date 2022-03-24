8f2aa431065e9b01d52c582b8b1c4d5ff646057f
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-03-24 23:28:22 +0900

    Time: 132 ms (30.77%), Space: 17.5 MB (86.00%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,29 +1,32 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # 1. is intervals empty? 
+        
+        #1. invervals가 비었는지 검사한다. 
         if not intervals:
             return 0
         
-        #무작위로 기입된 invervals를 회의 시작시간을 기준으로하여 정렬한다. 
-        intervals.sort(key= lambda x: x[0])
+        #2. intervals를 시작시간을 기준으로 정렬한다. 
+        # 시작시간 기준으로 되어있으므로, 
+        # 나중에 들어오는건 앞선 시작시간보다 뒤 임이 보장된다. 
+        # 따라서 시작한 회의의 종료시간과 새로운 회의의 시작시간의 순서를 비교하면된다. 
+        intervals.sort(key= lambda x : x[0])
         
+        #3. heapq에 제일 먼저 시작하는 회의의 종료시간을 넣는다. 
         free_rooms = []
-        
         heapq.heappush(free_rooms, intervals[0][1])
         
         for new_meeting in intervals[1:]:
-            # 새로운 미팅의 시작 시간이, 이전에 진행중이던 회의의 종료시간보다 
-            # 뒤 일경우, 겹치지 않으므로, 그 회의를 제거한다.             
-            if free_rooms[0] <= new_meeting[0]:
+            #새로운 미팅의 시작시간이 가장 빨리 끝나는 룸의 종료시간(free_rooms엔 종료시간이 들어감)보다 뒤라면
+            #이제 그 룸은 체킹 안해도되서 빼낸다. 
+            if new_meeting[0] >= free_rooms[0]:
                 heapq.heappop(free_rooms)
             
-            # 이전에 진행중이던 회의의 종료시간전에 신규 회의가 시작 될경우 
-            # 새로운 회의의 종료 시간(new_metting[1] 을 heapq에 넣는다. 
-            # 시작시간은 heaqp에 이전에 넣었던 회의의 종료시간과 비교하여 고려된다. 
-            # heapq의 deapth 자체가 필요한 회의룸의 개수가 된다.             
-            heapq.heappush(free_rooms, new_meeting[1])
-            
-        return len(free_rooms)
+            heapq.heappush(free_rooms,new_meeting[1])
             
         
+        return len(free_rooms)
+            
+                
+            
+            
         
