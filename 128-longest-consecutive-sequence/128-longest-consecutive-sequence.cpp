e07b0e3821f13bd82a3cce4288e22483093978d0
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-04-14 02:19:07 +0900

    Time: 67 ms (82.98%), Space: 30.9 MB (60.06%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,31 +1,40 @@
-#include <unordered_set>
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        // 아이디어
+        // 저 애들을 다 hashmap에 넣고, 
+        // 있으면 추가하면 된다. 
+        // 자료형으로 set을 사용할것,
+        // set 과 map의 차이는, 
+        // set은 해당 수의 존재 유무를 확인하고, 
+        // map은 key 와 value를 저장한다. 
         unordered_set<int> box;
-        for(int i=0; i<nums.size(); i++)
-            box.insert(nums[i]);     
-        
         int maxcnt=0;
-        while(box.size()>0){
-            int number=*box.begin();
-            int count=1;
+        for(auto a:nums)
+            box.insert(a);
+        
+        while(box.size()!=0){
+            int number = *box.begin();
             box.erase(number);
-            int bigger=number+1;
-            int smaller=number-1;
-            
-            while(box.count(smaller)>0){
-                box.erase(smaller--);                
-                count++;
-            }
-            while(box.count(bigger)>0){
-                box.erase(bigger++);
-                count++;                
+            int cnt=1;
+            // find left
+            int left=number;
+            while(box.find(--left)!=box.end()){                
+                cnt++;               
+                box.erase(left);
             }
             
-            maxcnt = max(maxcnt,count);
+            // find right
+           int right=number;
+            while(box.find(++right)!=box.end()){
+                cnt++;
+                box.erase(right);
+            }
+            maxcnt  = max(cnt,maxcnt);           
+            
         }
         return maxcnt;
+        
         
     }
 };
