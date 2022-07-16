8a0c32fed5d4ee723f68aa61c238ba5bbb568a85
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-07-16 15:00:12 +0900

    Time: 94 ms (5.02%), Space: 23.9 MB (29.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    
-    //문제, 가장 넓은 섬의 크기를 반환하시오
-    //dfs로 풀어야했고,
-    //grid[j][i]==1 를 재귀함수를 통해 따라 들어가서 for을 도는데, 
-    //재귀함수를 통해 들어간거 자체가 일단 grid[j][i]==1 이므로, return 할때 m+1을 해주어야 한다. 
-    // 그 이외에는 dfs를 통해 카운팅한 숫자 만큼을 더한다. 
-    // 
-        
-    int dfs(int sr, int sc, int h, int w, vector<vector<int>>& grid){
-         int dx[]={-1,0,1,0};
-        int dy[]={0,-1,0,1};
-        int x ; int y;
-        grid[sr][sc]=0;
-        int m=0;
-        for(int i=0; i<4; i++){
-            x=sc+dx[i];
-            y=sr+dy[i];            
-            if(x>=0 && y>=0 && x<w && y<h && grid[y][x]==1){
-                m+=dfs(y,x,h,w,grid);
-            }
-        }        
-        return m+1;
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-       
-        int h= grid.size();
-        int w= grid[0].size();
-        int maxArea=0;
-        for(int j=0; j<h; j++)
-            for(int i=0; i<w; i++){
+        
+        int maxarea=0;
+        
+        int h = grid.size();
+        int w = grid[0].size();
+        
+        unordered_map<int,int> m;
+        queue<pair<int,int>> q;
+        int max =0;
+        for(int j=0;  j<h; j++){
+            for(int i=0; i <w; i++){
                 if(grid[j][i]==1){
-                    maxArea=max(dfs(j,i,h,w,grid),maxArea);
-                    //cout << j << " : " << i << endl;
-                    
+                    q.push({j,i});
+                    int cnt=1;   
+                    grid[j][i]=0;
+                    cout << j<<":" <<i<<"\n";                 
+                    while(!q.empty()){
+                        int n,m;
+                        tie(n,m) = q.front();
+                        q.pop();
+                               
+                        if(m+1<w && grid[n][m+1]==1) {q.push({n,m+1}); grid[n][m+1]=0; cnt++; cout << n<<":" <<m+1<<"\n";}
+                        if(n+1<h && grid[n+1][m]==1) {q.push({n+1,m}); grid[n+1][m]=0; cnt++; cout << n+1<<":" <<m<<"\n";}
+                        if(n-1>=0 && grid[n-1][m]==1) {q.push({n-1,m}); grid[n-1][m]=0; cnt++; cout << n-1<<":" <<m<<"\n";}
+                        if(m-1>=0 && grid[n][m-1]==1) {q.push({n,m-1}); grid[n][m-1]=0; cnt++; cout << n-1<<":" <<m<<"\n";}
+                    }
+                    if (max<cnt)max=cnt;
+                    cout << "\n";
                 }
             }
-        return maxArea;    
+        }
+        
+        return max;
+        
     }
-    
 };
