da2e90454a2bc8ef5507b94fd3fd963b988971d1
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-08-27 06:38:12 +0900

    Time: 82 ms (62.03%), Space: 14.2 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote)>len(magazine) : return False
+        
+        magazine_counts = collections.Counter(magazine)
+        ransome_note_counts = collections.Counter(ransomNote)
+        
+        for char,count in ransome_note_counts.items():
+            magazine_count = magazine_counts[char]
+            if magazine_count < count:
+                return False
+            
+        return True
+        
