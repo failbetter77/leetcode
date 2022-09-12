abd902960caa47ed43fe1c5138fd1e682d6d004f
failbetter77 <100422523+failbetter77@users.noreply.github.com>
2022-09-13 00:29:27 +0900

    Time: 3265 ms (57.14%), Space: 53.9 MB (71.43%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -4,14 +4,9 @@ class Solution:
         
         pq = []
         heapq.heappush(pq,intervals[0][1]) ## push end time 
-        ret = 1
-        retm=0
         for s, e in intervals[1:]:
             if s > pq[0]:
-                heapq.heappop(pq)
-                ret-=1
-            else :                
-                ret+=1
+                heapq.heappop(pq)            
             heapq.heappush(pq,e)
         return len(pq)
             
